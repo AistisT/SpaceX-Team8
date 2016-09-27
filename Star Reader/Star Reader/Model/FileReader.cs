016c2c161f66f0b3663d72753a2b29b44bd89f5c
suzannebuchanan <s.z.buchanan.dundee.ac.uk>
2016-09-27 15:40:21 +0100

    stuff

--- Star Reader/Star Reader/Model/FileReader.cs
+++ Star Reader/Star Reader/Model/FileReader.cs
@@ -50,6 +50,7 @@ namespace Star_Reader.Model
                         break;
                 }
             }
+
             r.testForBabblingIdiot();
             return r;
         }//end StoreRecording
