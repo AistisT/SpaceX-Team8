229d86c99a7432f56712f94c5cdc41d7a95b7bce
Alan <alanchuntington@hotmail.com>
2016-09-29 22:57:35 +0100

    Added quick global exception handling, Any unhandled exception will be caught and not crash the program, left it with a default message that isnt all that helpful but atleast tells you more than oops something went wrong.

--- Star Reader/Star Reader/App.xaml.cs
+++ Star Reader/Star Reader/App.xaml.cs
@@ -11,5 +11,11 @@ namespace Star_Reader
     {
         // Dictionary to hold open ports data
         public static Dictionary<int, Recording> RecordingData = new Dictionary<int, Recording>();
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
